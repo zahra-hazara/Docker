965a271759e35b8d96e3f54bc4f383021d62333f
zahra <zahra.hazara@metropolia.fi>
2024-09-27 12:49:50 +0300

    tunti_tehtävä

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,27 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator= new Calculator();
+
+    @Test
+    public void multiplyTest(){
+        assertEquals(10, calculator.multiply(5,2));
+    }
+
+    @Test
+    public void subtractTest(){
+        assertEquals(3, calculator.subtract(5,2));
+    }
+
+    @Test
+    public void divideTest(){
+        assertEquals(3, calculator.divide(6,2));
+    }
+}
+
+
+
